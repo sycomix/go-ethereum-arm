6f9b4ea4d7fe443a2edd412c71f87afd18a1ca4a
Christopher <chrishen@hku.hk>
2018-09-17 16:50:49 +0800

    Using version sort

--- check-version.sh
+++ check-version.sh
@@ -4,4 +4,4 @@ if [ -f ./version_list ]; then
     rm version_list
 fi
 
-curl -s "https://github.com/ethereum/go-ethereum/releases" | grep "/ethereum/go-ethereum/releases/tag/" | grep -v '\-rc' | sed 's/.*tag\/\(.*\)\">.*/\1/' >> version_list
+curl -s "https://github.com/ethereum/go-ethereum/releases" | grep "/ethereum/go-ethereum/releases/tag/" | grep -v '\-rc' | sed 's/.*tag\/\(.*\)\">.*/\1/' | sort -rV >> version_list
--- latest-tag.sh
+++ latest-tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LATEST=$(git tag | sort -r | head -n 1)
+LATEST=$(git tag | sort -rV | head -n 1)
 
 if [ "${LATEST}" == "${CI_COMMIT_TAG}" ]; then 
     docker tag ${DOCKER_NS}/${BASENAME}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:latest;
