43ea60b0d507bb390f2db1abab1bc670de6fb65f
Christopher <chrishen@hku.hk>
2018-09-17 18:09:50 +0800

    Use stable tag instead of latest

--- latest-tag.sh
+++ latest-tag.sh
@@ -3,6 +3,6 @@
 LATEST=$(git tag | sort -rV | head -n 1)
 
 if [ "${LATEST}" == "${CI_COMMIT_TAG}" ]; then 
-    docker tag ${DOCKER_NS}/${BASENAME}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:latest;
-    docker tag ${DOCKER_NS}/${BASENAME}:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:alltools;
+    docker tag ${DOCKER_NS}/${BASENAME}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:stable;
+    docker tag ${DOCKER_NS}/${BASENAME}:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:alltools-stable;
 fi
