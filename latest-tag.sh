85c3f182587634a3b997f720b1d2dab4b233b35b
Christopher <chrishen@hku.hk>
2018-09-20 17:17:26 +0800

    Add little DAG version for mining at low end devices on private network

--- latest-tag.sh
+++ latest-tag.sh
@@ -5,4 +5,6 @@ LATEST=$(git tag | sort -rV | head -n 1)
 if [ "${LATEST}" == "${CI_COMMIT_TAG}" ]; then 
     docker tag ${DOCKER_NS}/${BASENAME}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:stable;
     docker tag ${DOCKER_NS}/${BASENAME}:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:alltools-stable;
+    docker tag ${DOCKER_NS}/${BASENAME}-little-DAG:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-DAG:stable;
+    docker tag ${DOCKER_NS}/${BASENAME}-little-DAG:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-DAG:alltools-stable;
 fi
