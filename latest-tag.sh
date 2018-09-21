ee7615e6a59da5444bae86e1eaffd264ac003363
Christopher <chrishen@hku.hk>
2018-09-21 10:29:24 +0800

    Fix last-tag.sh

--- latest-tag.sh
+++ latest-tag.sh
@@ -3,8 +3,8 @@
 LATEST=$(git tag | sort -rV | head -n 1)
 
 if [ "${LATEST}" == "${CI_COMMIT_TAG}" ]; then 
-    docker tag ${DOCKER_NS}/${BASENAME}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:stable;
-    docker tag ${DOCKER_NS}/${BASENAME}:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:alltools-stable;
-    docker tag ${DOCKER_NS}/${BASENAME}-little-dag:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-dag:stable;
-    docker tag ${DOCKER_NS}/${BASENAME}-little-dag:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-dag:alltools-stable;
+    docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-${ARMVER}:stable;
+    docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-${ARMVER}:alltools-stable;
+    docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}-little-dag:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-dag:stable;
+    docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}-little-dag:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-dag:alltools-stable;
 fi
