9ac10fea3666a145c0616176ab7207a62fc79c70
Christopher <chrishen@hku.hk>
2018-09-21 11:15:22 +0800

    Separate little-dag to standalone stages

--- latest-tag.sh
+++ latest-tag.sh
@@ -5,6 +5,4 @@ LATEST=$(git tag | sort -rV | head -n 1)
 if [ "${LATEST}" == "${CI_COMMIT_TAG}" ]; then 
     docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-${ARMVER}:stable;
     docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-${ARMVER}:alltools-stable;
-    docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}-little-dag:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-dag:stable;
-    docker tag ${DOCKER_NS}/${BASENAME}-${ARMVER}-little-dag:alltools-${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}-little-dag:alltools-stable;
 fi
